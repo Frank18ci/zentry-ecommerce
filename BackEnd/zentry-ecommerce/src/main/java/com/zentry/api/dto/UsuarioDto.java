3317e54fd77c6088b97742d7c05c5de7ce461f0c
FrankCarpio <fcarpio113@gmail.com>
2025-06-02 13:04:55 -0500

    Cambio tipo de dato direccion por no requerido para registro

--- BackEnd/zentry-ecommerce/src/main/java/com/zentry/api/dto/UsuarioDto.java
+++ BackEnd/zentry-ecommerce/src/main/java/com/zentry/api/dto/UsuarioDto.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.zentry.api.model.Direccion;
 import com.zentry.api.model.Rol;
 import com.zentry.api.model.Usuario;
 
@@ -30,30 +31,33 @@ public class UsuarioDto {
 	public static UsuarioDto usuarioToUsuarioDto(Usuario usuario) {
 		return UsuarioDto.builder()
 				.id(usuario.getId())
-				.rol(RolDto.listRolToListRolDto(usuario.getRol()))
+				.rol(RolDto.listRolToListRolDto(usuario.getRol() != null ? usuario.getRol(): List.of() ))
 				.nombre(usuario.getNombre())
 				.apellido(usuario.getApellido())
 				.correoElectronico(usuario.getCorreoElectronico())
 				.contraseña(usuario.getContraseña())
 				.telefono(usuario.getTelefono())
 				.fechaCreacion(usuario.getFechaCreacion())
-				.direccion(DireccionDto.direccionToDireccionDto(usuario.getDireccion()))
+				.direccion(DireccionDto.direccionToDireccionDto(usuario.getDireccion() != null ? usuario.getDireccion() : Direccion.builder().build()))
 				.build();
 	}
 	public static List<UsuarioDto> listUsuarioToListUsuarioDto(List<Usuario> usuarios){
 		return usuarios.stream().map(UsuarioDto::usuarioToUsuarioDto).collect(Collectors.toList());
 	}
 	public static Usuario usuarioDtoToUsuario(UsuarioDto usuarioDto) {
-		return Usuario.builder()
+		Usuario usuario = Usuario.builder()
 				.id(usuarioDto.getId())
-				.rol(RolDto.listRolDTOToListRol(usuarioDto.getRol()))
+				.rol(RolDto.listRolDTOToListRol(usuarioDto.getRol() != null ? usuarioDto.getRol(): List.of()))
 				.nombre(usuarioDto.getNombre())
 				.apellido(usuarioDto.getApellido())
 				.correoElectronico(usuarioDto.getCorreoElectronico())
 				.contraseña(usuarioDto.getContraseña())
 				.telefono(usuarioDto.getTelefono())
 				.fechaCreacion(usuarioDto.getFechaCreacion())
-				.direccion(DireccionDto.direccionDtoToDireccion(usuarioDto.getDireccion()))
 				.build();
+		if (usuarioDto.getDireccion() != null && usuarioDto.getDireccion().getId() != null) {
+			usuario.setDireccion(DireccionDto.direccionDtoToDireccion(usuarioDto.getDireccion()));
+		}
+		return usuario;
 	}
 }
